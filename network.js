0cfd828b0ad7b570be294b0e0043a7f05f95ce98
Sturla Bragason <sturla.bragason@devoteam.com>
2023-02-04 10:08:52 +0000

    fix

--- network.js
+++ network.js
@@ -340,15 +340,15 @@ function draw() {
 
     // create a network
     var container = document.getElementById("mynetwork");
-    container.style.width = '100%';
-    container.style.height = '100%';
+    // container.style.width = '100%';
+    // container.style.height = '100%';
     var data = {
         nodes: nodes,
         edges: edges,
     };
     var options = {
-        width: "100%",
-        height: "100%",
+        // width: "100%",
+        // height: "100%",
         nodes: {
             shape: "dot",
             size: 16,
